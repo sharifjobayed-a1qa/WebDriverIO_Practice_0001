0c6ce6c788b4118a45714e50805fa2d6de5aa2dc
s5.islam <s5.islam@a1qa.com>
2024-04-01 17:25:49 +0600

    Working on BaseElement.js

---
+++ test/specs/test.e2e.js
@@ -0,0 +1,15 @@
+import { beforeEach } from 'mocha';
+
+
+beforeEach(``,async()=>{
+    await browser.setWindowSize(1440,900);
+    await browser.url(`https://the-internet.herokuapp.com`);
+});
+
+
+describe('', async() => {
+    it('', async () => {
+
+    })
+})
+
