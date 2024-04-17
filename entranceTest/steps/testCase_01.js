b3acd2c72929431ffc0fcca594af5346761d7136
s5.islam <s5.islam@a1qa.com>
2024-04-17 15:52:28 +0600

    `entranceTest\userinyerface.feature` edited.

--- entranceTest/steps/testCase_01.js
+++ entranceTest/steps/testCase_01.js
@@ -3,9 +3,9 @@ import {expect} from '@wdio/globals';
 
 
 Given(`I'm on the Home page`, async() => {});
-When(`I click the HERE link to navigate the next page.`, async() => {});
-Then(`The '1' card is open.`, async() => {});
-When(`I input random valid password, email, accept the terms of use and click "next" button.`, async() => {});
-Then(`The '2' card is open.`, async() => {});
-When(`I Choose 3 random interests, click "Next" button.`, async() => {});
-Then(`The expected result is, Text 'Please upload a picture' appear, text color is green.`, async() => {});
+When(`I click the HERE link to navigate the next page`, async() => {});
+Then(`The '1' card is open`, async() => {});
+When(`I input random valid password, email, accept the terms of use and click "next" button`, async() => {});
+Then(`The '2' card is open`, async() => {});
+When(`I Choose 3 random interests, click "Next" button`, async() => {});
+Then(`The expected result is, Text 'Please upload a picture' appear, text color is green`, async() => {});
